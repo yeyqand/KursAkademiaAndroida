78cda347eab2bcc3f1cccb44262356f7b116b525
rzak <rzak@alan-systems.com>
2020-08-27 19:44:06 +0200

    Fixes after modul8 review

--- app/src/main/java/com/yeyq/kursakademiaandroida/features/characters/presentation/CharacterFragment.kt
+++ app/src/main/java/com/yeyq/kursakademiaandroida/features/characters/presentation/CharacterFragment.kt
@@ -52,7 +52,6 @@ class CharacterFragment : BaseFragment<CharacterViewModel>(R.layout.fragment_cha
     private fun observeCharacters() {
         viewModel.characters.observe(this) {
             showCharacters(it)
-            // display characters
         }
     }
 
